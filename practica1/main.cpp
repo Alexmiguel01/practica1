1b4c82e3df89c4ef7dc16c2b8033dbfe5679aa0e
CEL <CEL@Internet-2>
2014-05-25 10:46:09 -0500

    Creaciòn del proyecto

---
+++ practica1/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: CEL
+ *
+ * Created on 25 de mayo de 2014, 10:45
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
